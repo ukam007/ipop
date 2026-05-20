bfce01655e3719d9647700d152d6758a2076d358
DBB <2009duanbing@163.com>
2026-05-20 11:50:25 +0800

    v1.0.34: fix enter key newline display issue

--- src/terminal/manager.ts
+++ src/terminal/manager.ts
@@ -103,8 +103,9 @@ export class TerminalManager implements vscode.Pseudoterminal {
             return;
         }
 
-        if (data === '\r') {
-            const inputLen = this.inputBuffer.length;
+if (data === '\r') {
+            this.writeEmitter.fire('\r');
+            this.writeEmitter.fire('\x1b[2K');
             
             if (this.logger) {
                 this.logger.logInput(this.inputBuffer);
@@ -113,12 +114,6 @@ export class TerminalManager implements vscode.Pseudoterminal {
                 getHistory().recordCommand(this.inputBuffer, 'user');
             } catch {}
             this.client.send(this.inputBuffer);
-            
-            // 删除原始命令
-            for (let i = 0; i < inputLen; i++) {
-                this.writeEmitter.fire('\b \b');
-            }
-            this.writeEmitter.fire('\r\n');
             
             this.inputBuffer = '';
             this.hintShown = false;
